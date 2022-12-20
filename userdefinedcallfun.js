d1a5f68d2067f9c9c8891e0c1ac7768e6cd4b9ad
David Prakash <118829009+davidprakash12@users.noreply.github.com>
2022-12-20 16:35:48 +0530

    Add files via upload

---
+++ userdefinedcallfun.js
@@ -0,0 +1,17 @@
+1) Design a function, print message and assign the function to a variable and print it like a function
+<html lang="en">
+<head>
+    <script>
+        function hello(){
+            document.write("Hello <Br />")
+        }
+
+        hello();
+        a=hello;
+        a();
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
