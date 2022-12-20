2783925b013f982734b60e76bff931afb19eddfa
David Prakash <118829009+davidprakash12@users.noreply.github.com>
2022-12-20 16:39:53 +0530

    Add files via upload

--- userdefinedcallfun.js
+++ userdefinedcallfun.js
@@ -15,3 +15,63 @@
     
 </body>
 </html>
+//
+<html lang="en">
+<head>
+    <script>
+        hello();
+        function hello(){
+            document.write("Hello <Br />")
+        }
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
+
+<html lang="en">
+<head>
+    <script>
+        function hello(){
+            document.write("Hello <Br />")
+        }
+    </script>
+</head>
+<body>
+    <script>
+        hello();
+    </script>
+</body>
+</html>
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+<html lang="en">
+<head>
+    <script>
+        hello(); 
+    </script>
+</head>
+<body>
+    <script>
+        function hello(){
+            document.write("Hello <Br />")
+        }
+    </script>
+</body>
+</html>
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+<html lang="en">
+<head>
+    <script>
+         
+    </script>
+</head>
+<body>
+    <script>
+        hello();
+        function hello(){
+            document.write("Hello <Br />")
+        }
+    </script>
+</body>
+</html>
